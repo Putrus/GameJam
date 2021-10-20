7b09e0a7cd0189299dcf6017aca3f04b134565d4
Panven <kacper.tylski.szkola@gmail.com>
2021-10-20 13:48:02 +0200

    Field class init

---
+++ GameJam/Field.cpp
@@ -0,0 +1,5 @@
+#pragma once
+#include "Field.h"
+Field::Field() {
+
+}
---
+++ GameJam/Field.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <SFML/Graphics/Sprite.hpp>
+
+class Field : public sf::Sprite {
+
+public:
+	
+
+private:
+	
+
+public: 
+	
+
+};
