11af950ac2655a366d8802a40f65b77975575c0d
Panven <kacper.tylski.szkola@gmail.com>
2021-10-20 15:00:07 +0200

    Destructor

--- GameJam/Background.cpp
+++ GameJam/Background.cpp
@@ -2,9 +2,9 @@
 
 Background::Background(){}
 Background::Background(TextureHolder& textures) {
-	for (size_t i = 0; i < fields.size(); ++i) {
+	for (size_t i = 0; i < 8; ++i) {
 		fields.push_back(std::vector<Field*>());
-		for (size_t j = 0; j < fields.size(); ++j) {
+		for (size_t j = 0; j < 8; ++j) {
 			Field *field = new Field();
 			field->setTexture(textures.get(Textures::b1));
 			field->setPosition(i * 96, j * 96);
@@ -12,7 +12,13 @@ Background::Background(TextureHolder& textures) {
 		}
 	}
 }
-
+Background::~Background() {
+	for (size_t i = 0; i < 8; ++i) {
+		for (size_t j = 0; j < 8; ++j) {			
+			delete(fields[i][j]);
+		}
+	}
+}
 void Background::draw(sf::RenderWindow& window) {
 	float x = 0, y = 0;
 	for (size_t i = 0; i < fields.size(); ++i) {
