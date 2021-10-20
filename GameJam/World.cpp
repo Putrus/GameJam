9f6ab7b1a6cba326c3fc76add7f90fd9a255786a
Putrus <puertaputra@gmail.com>
2021-10-20 13:45:16 +0200

    Delete unecessary changes

--- GameJam/World.cpp
+++ GameJam/World.cpp
@@ -10,13 +10,6 @@ void World::update(sf::Time dt) {
 
 void World::draw() {
 	//background.draw(mWindow);
-	for (int i = 0; i < 8; ++i) {
-		for (int j = 0; j < 8; ++j) {
-			sf::Sprite sprite(mTextures.get(Textures::b1));
-			sprite.setPosition(i * 96, j * 96);
-			mWindow.draw(sprite);
-		}
-	}
 }
 
 void World::loadTextures() {
