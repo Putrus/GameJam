2f9fa6e58086f347e4beb87661f723212d9214f3
Panven <kacper.tylski.szkola@gmail.com>
2021-10-20 19:08:57 +0200

    Character movement

--- GameJam/Game.cpp
+++ GameJam/Game.cpp
@@ -36,28 +36,49 @@ void Game::processEvents()
 		case sf::Event::Closed:
 			mWindow.close();
 			break;
-		case sf::Event::KeyPressed:
-			if(event.key.code == sf::Keyboard::W){
-				character.move(Up);
-			}
-			if (event.key.code == sf::Keyboard::S) {
-				character.move(Down);
-			}
-			if (event.key.code == sf::Keyboard::A) {
-				character.move(Left);
-			}
-			if (event.key.code == sf::Keyboard::D) {
-				character.move(Right);
-			}
-			break;
+		
 		case sf::Event::KeyReleased:
-			if (event.key.code == sf::Keyboard::W
-				|| event.key.code == sf::Keyboard::S
-				|| event.key.code == sf::Keyboard::A
-				|| event.key.code == sf::Keyboard::D) {
+			if (event.key.code == sf::Keyboard::W && character.getSpeed().y < 0
+				|| event.key.code == sf::Keyboard::S && character.getSpeed().y > 0
+				|| event.key.code == sf::Keyboard::A && character.getSpeed().x < 0
+				|| event.key.code == sf::Keyboard::D && character.getSpeed().x > 0) {
 				character.move(Stop);
 			}
 			break;
+		case sf::Event::KeyPressed:			
+			if(event.key.code == sf::Keyboard::W){
+				if (character.getSpeed().x < 0)
+					character.move(UpLeft);
+				else if(character.getSpeed().x > 0)
+					character.move(UpRight);
+				else
+					character.move(Up);
+			}
+			if (event.key.code == sf::Keyboard::S) {
+				if (character.getSpeed().x < 0)
+					character.move(DownLeft);
+				else if (character.getSpeed().x > 0)
+					character.move(DownRight);
+				else
+					character.move(Down);
+			}
+			if (event.key.code == sf::Keyboard::A) {
+				if (character.getSpeed().y < 0)
+					character.move(UpLeft);
+				else if (character.getSpeed().y > 0)
+					character.move(DownLeft);
+				else
+				character.move(Left);
+			}
+			if (event.key.code == sf::Keyboard::D) {
+				if (character.getSpeed().y < 0)
+					character.move(UpRight);
+				else if (character.getSpeed().y > 0)
+					character.move(DownRight);
+				else
+				character.move(Right);
+			}			
+			break;		
 		}
 	}
 }
